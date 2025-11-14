8fd25a5c0842b45a6f2a254f2e2e306f9f2c1fb0
Febrian Arka Samudra <febrianarka10@gmail.com>
2025-11-14 17:40:26 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/stream_arka/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.stream_arka
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/stream.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ColorStream{
+
+  final List<Color> colors = [
+    Colors.blueGrey,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal,
+    Colors.lightGreen,
+    Colors.pink,
+    Colors.blueAccent,
+    Colors.cyan,
+    Colors.purple
+  ];
+}
